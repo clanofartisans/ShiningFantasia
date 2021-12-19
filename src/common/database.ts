f0d680448077a381d5f3feb8c639893e8ff07031
Velyn <ffxi@somepage.org>
2021-12-18 21:51:55 -0800

    Update resource descriptions

--- src/common/database.ts
+++ src/common/database.ts
@@ -88,56 +88,56 @@ const commonResources: ResourceEntry[] = [
     { fileId: 0xD987,  type: ResourceType.Dmsg,         description: 'Chat Window Command Help Text' }, // 35
     { fileId: 0xD98A,  type: ResourceType.Dmsg,         description: 'Monster Family Names' }, // 36
     { fileId: 0xD98B,  type: ResourceType.Dmsg,         description: 'Moblin Maze Mongers Rune Help Text' }, // 37
-    { fileId: 0x5B,    type: ResourceType.Item,         description: 'Item 65535' }, // 38
+    { fileId: 0x5B,    type: ResourceType.Item,         description: 'Gil - Item 65535' }, // 38
     { fileId: 0x49,    type: ResourceType.Item,         description: 'Items 0 - 4095' }, // 39
     { fileId: 0xD977,  type: ResourceType.Item,         description: 'Items 8704 - 10239' }, // 40
     { fileId: 0x4A,    type: ResourceType.Item,         description: 'Items 4096 - 8191' }, // 41
-    { fileId: 0x4B,    type: ResourceType.Item,         description: 'Items 16384 - 23039' }, // 42
-    { fileId: 0xD973,  type: ResourceType.Item,         description: 'Items 28672 - 29695' }, // 43
-    { fileId: 0xD976,  type: ResourceType.Item,         description: 'Items 29696 - 30719' }, // 44
+    { fileId: 0x4B,    type: ResourceType.Item,         description: 'Weapons - Items 16384 - 23039' }, // 42
+    { fileId: 0xD973,  type: ResourceType.Item,         description: 'Moblin Maze Mongers - Items 28672 - 29695' }, // 43
+    { fileId: 0xD976,  type: ResourceType.Item,         description: 'Monstronsity - Items 29696 - 30719' }, // 44
     { fileId: 0x5F,    type: ResourceType.Item,         description: 'Items 61432 - 61439' }, // 45
-    { fileId: 0xD978,  type: ResourceType.Item,         description: 'Items 57344 - 61431' }, // 46
-    { fileId: 0xD979,  type: ResourceType.Item,         description: 'Items 61952 - 62975' }, // 47
+    { fileId: 0xD978,  type: ResourceType.Item,         description: 'Records of Eminence Objectives - Items 57344 - 61431' }, // 46
+    { fileId: 0xD979,  type: ResourceType.Item,         description: 'Records of Eminence Categories - Items 61952 - 62975' }, // 47
     { fileId: 0xD97D,  type: ResourceType.Item,         description: 'Items 63024 - 63263' }, // 48
     { fileId: 0xD97E,  type: ResourceType.Item,         description: 'Items 63008 - 63023' }, // 49
     { fileId: 0xD8D0,  type: ResourceType.Item,         description: 'Items 62976 - 62995' }, // 50
-    { fileId: 0x4C,    type: ResourceType.Item,         description: 'Items 10240 - 16383' }, // 51
-    { fileId: 0xD974,  type: ResourceType.Item,         description: 'Items 23040 - 28671' }, // 52
-    { fileId: 0x4D,    type: ResourceType.Item,         description: 'Items 8192 - 8703' }, // 53
+    { fileId: 0x4C,    type: ResourceType.Item,         description: 'Armor - Items 10240 - 16383' }, // 51
+    { fileId: 0xD974,  type: ResourceType.Item,         description: 'Armor - Items 23040 - 28671' }, // 52
+    { fileId: 0x4D,    type: ResourceType.Item,         description: 'Puppetmaster Automaton - Items 8192 - 8703' }, // 53
     { fileId: 0x55,    type: ResourceType.Dmsg,         description: '<Unknown>' }, // 54
     { fileId: 0x56,    type: ResourceType.Dmsg,         description: '<Unknown>' }, // 55
     { fileId: 0x57,    type: ResourceType.Dmsg,         description: '<Unknown>' }, // 56
     { fileId: 0x9A85,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 57
-    { fileId: 0xD975,  type: ResourceType.Item,         description: 'Items 61440 - 61951' }, // 58
-    { fileId: 0xD97F,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 59 / 26
+    { fileId: 0xD975,  type: ResourceType.Item,         description: 'Monstronsity - Items 61440 - 61951' }, // 58
+    { fileId: 0xD97F,  type: ResourceType.Dmsg,         description: 'The 15th Anniversary Quiz for the Ages' }, // 59 / 26
     { fileId: 0xD998,  type: ResourceType.Dmsg,         description: 'Titles' }, // 60
     { fileId: 0xD999,  type: ResourceType.Dmsg,         description: 'Key Items' }, // 61
     { fileId: 0xD99A,  type: ResourceType.Dmsg,         description: 'Quests - San d\'Oria' }, // 62
     { fileId: 0xD99B,  type: ResourceType.Dmsg,         description: 'Quests - Bastok' }, // 63
     { fileId: 0xD99C,  type: ResourceType.Dmsg,         description: 'Quests - Windurst' }, // 64
-    { fileId: 0xD99D,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 65
-    { fileId: 0xD99E,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 66
-    { fileId: 0xD99F,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 67
-    { fileId: 0xD9A0,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 68
-    { fileId: 0xD9AA,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 69
-    { fileId: 0xD9A1,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 70
-    { fileId: 0xD9A3,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 71
-    { fileId: 0xD9A4,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 72
-    { fileId: 0xD9A5,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 73
-    { fileId: 0xD9A6,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 74
-    { fileId: 0xD9A7,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 75
-    { fileId: 0xD9A8,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 76
-    { fileId: 0xD9A9,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 77
-    { fileId: 0xD9AC,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 78
-    { fileId: 0xD9AB,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 79
-    { fileId: 0xD9B7,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 80
-    { fileId: 0xD9B8,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 81
-    { fileId: 0xD9B9,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 82
-    { fileId: 0xD9BA,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 83
-    { fileId: 0xD9BD,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 84
+    { fileId: 0xD99D,  type: ResourceType.Dmsg,         description: 'Quests - Jeuno' }, // 65
+    { fileId: 0xD99E,  type: ResourceType.Dmsg,         description: 'Quests - Other Areas' }, // 66
+    { fileId: 0xD99F,  type: ResourceType.Dmsg,         description: 'Quests - Outlands' }, // 67
+    { fileId: 0xD9A0,  type: ResourceType.Dmsg,         description: 'Quests - Treasures of Aht Urhgan' }, // 68
+    { fileId: 0xD9AA,  type: ResourceType.Dmsg,         description: 'Quests - Wings of the Goddess' }, // 69
+    { fileId: 0xD9A1,  type: ResourceType.Dmsg,         description: 'Quests - Abyssea' }, // 70
+    { fileId: 0xD9A3,  type: ResourceType.Dmsg,         description: 'Missions - San d\'Oria' }, // 71
+    { fileId: 0xD9A4,  type: ResourceType.Dmsg,         description: 'Missions - Bastok' }, // 72
+    { fileId: 0xD9A5,  type: ResourceType.Dmsg,         description: 'Missions - Windurst' }, // 73
+    { fileId: 0xD9A6,  type: ResourceType.Dmsg,         description: 'Missions - Rise of the Zilart' }, // 74
+    { fileId: 0xD9A7,  type: ResourceType.Dmsg,         description: 'Missions - Chains of Promathia' }, // 75
+    { fileId: 0xD9A8,  type: ResourceType.Dmsg,         description: 'Quests - Assult' }, // 76
+    { fileId: 0xD9A9,  type: ResourceType.Dmsg,         description: 'Missions - Treasures of Aht Urhgan' }, // 77
+    { fileId: 0xD9AC,  type: ResourceType.Dmsg,         description: 'Quests - Campaign Ops' }, // 78
+    { fileId: 0xD9AB,  type: ResourceType.Dmsg,         description: 'Missions - Wings of the Goddess' }, // 79
+    { fileId: 0xD9B7,  type: ResourceType.Dmsg,         description: 'Missions - A Crystalline Prophecy' }, // 80
+    { fileId: 0xD9B8,  type: ResourceType.Dmsg,         description: 'Missions - A Moogle Kupo d\'Etat' }, // 81
+    { fileId: 0xD9B9,  type: ResourceType.Dmsg,         description: 'Missions - A Shantotto Ascension' }, // 82
+    { fileId: 0xD9BA,  type: ResourceType.Dmsg,         description: 'Missions - Seekers of Adoulin' }, // 83
+    { fileId: 0xD9BD,  type: ResourceType.Dmsg,         description: 'Missions - Rhapsodies of Vana\'diel' }, // 84
     { fileId: 0xD9BE,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 85
-    { fileId: 0xD9BB,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 86
-    { fileId: 0xD9BC,  type: ResourceType.Dmsg,         description: '<Unknown>' }, // 87
+    { fileId: 0xD9BB,  type: ResourceType.Dmsg,         description: 'Quests - Seekers of Adoulin' }, // 86
+    { fileId: 0xD9BC,  type: ResourceType.Dmsg,         description: 'Quests - Coalition Assignments' }, // 87
     { fileId: 0x52,    type: ResourceType.Dmsg,         description: 'Resource - <Unknown>' }, // 88
     { fileId: 0x51,    type: ResourceType.Dmsg,         description: 'Resource - <Unknown>' }, // 89
     { fileId: 0xD9B4,  type: ResourceType.Dmsg,         description: 'Status Names with Adjectives' }, // 90
